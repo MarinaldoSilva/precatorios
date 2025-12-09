41834370b53d9b49be68d7879ddcb834127438eb
Mario <marinaldo12@hotmail.com>
2025-12-09 16:59:55 -0300

    correção das posições da urls

--- ativos/urls.py
+++ ativos/urls.py
@@ -8,7 +8,7 @@ router = DefaultRouter()
 router.register(r"", PrecatorioViewSet, basename="precatorios")
 
 urlpatterns = [ 
-    path("", include(router.urls)),
-    path("aprovacao/", PropostaPrecatorioAPIView.as_view(), name="aprovacao"),
-    path("aprovacao-bulk/", AprovarPrecatorioBulkView.as_view(), name="aprovacao-bulk")
+    path("proposta/", PropostaPrecatorioAPIView.as_view(), name="proposta"),
+    path("aprovacao-bulk/", AprovarPrecatorioBulkView.as_view(), name="aprovacao-bulk"),
+    path("", include(router.urls))
 ]
