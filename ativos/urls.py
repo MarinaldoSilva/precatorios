39c6847f3ccb4b6bb62419d3c967374c52be540d
Mario <marinaldo12@hotmail.com>
2025-12-10 14:04:14 -0300

    criação das rotas

--- ativos/urls.py
+++ ativos/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import PrecatorioViewSet, AprovarPrecatorioBulkView, PropostaPrecatorioAPIView
+from .views import PrecatorioViewSet, AprovarPrecatorioBulkView, PropostaPrecatorioAPIView, GerenciarPropostaViewAPIView
 
 router = DefaultRouter()
 
@@ -9,6 +9,8 @@ router.register(r"", PrecatorioViewSet, basename="precatorios")
 
 urlpatterns = [ 
     path("proposta/", PropostaPrecatorioAPIView.as_view(), name="proposta"),
-    path("aprovacao-bulk/", AprovarPrecatorioBulkView.as_view(), name="aprovacao-bulk"),
+    path("aprovacao/bulk/", AprovarPrecatorioBulkView.as_view(), name="aprovacao-bulk"),
+
+    path("gerenciar/propostas/", GerenciarPropostaViewAPIView.as_view(), name="gerenciar-propostas"),
     path("", include(router.urls))
 ]
