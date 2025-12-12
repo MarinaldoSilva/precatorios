56d9fd70305bc0ecbe7950054c0c72fc192c8bb7
Mario <marinaldo12@hotmail.com>
2025-12-12 15:51:32 -0300

    serializer agora mostra o lucro esperado e porcentagem do lucro, além de redução dos campos expostos no get

--- ativos/serializer.py
+++ ativos/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from decimal import Decimal
 from user.serializer import UserSerializer
 
 from .models import Precatorio, Proposta
@@ -8,11 +8,34 @@ from .models import Precatorio, Proposta
 class PrecatorioSerializer(serializers.ModelSerializer):
     dono = UserSerializer(read_only=True)
 
+    lucro_esperado = serializers.SerializerMethodField()
+    percentual_lucro = serializers.SerializerMethodField()
     class Meta:
         model = Precatorio
-        fields = "__all__"
+        fields = [
+            "id",
+            "titulo",
+            "valor_face",
+            "valor_inicial",
+            "tribunal",
+            "status",
+            "dono"
+        ]
 
         read_only_fields = ["id", "dono", "status"]
+
+    def get_lucro_esperado(self, obj: Precatorio):
+        return obj.valor_face - obj.valor_inicial
+    
+    def get_percentual_lucro(self, obj: Precatorio):
+        if obj.valor_face > 0:
+            lucro_percentual = ((obj.valor_face - obj.valor_inicial) / (obj.valor_face) * 100)
+            calculo = round(lucro_percentual, 2)
+
+            if calculo % 1 == 0:
+                return int(calculo)
+            return calculo
+        return None
 
     def create(self, validated_data):
         user = self.context["request"].user
@@ -23,16 +46,23 @@ class PrecatorioSerializer(serializers.ModelSerializer):
 class PropostaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Proposta
-        fields = ["id", "precatorio", "valor_oferta", "data_criacao", "status"]
+        fields = [
+            "id",
+            "precatorio",
+            "valor_oferta",
+            "data_criacao",
+            "status"
+            ]
+        
         read_only_fields = ["id", "investidor", "data_criacao", "status"]
 
     def validate(self, data):
-        precatorio_obj = data["precatorio"]
+        precatorio = data["precatorio"]
         valor_da_oferta = data["valor_oferta"]
 
-        if precatorio_obj.status != Precatorio.Status.DISPONIVEL:
+        if precatorio.status != Precatorio.Status.DISPONIVEL:
             raise serializers.ValidationError({"error": "O precatório não disponivel para compra"})
-        if valor_da_oferta >= precatorio_obj.valor_face:
+        if valor_da_oferta >= precatorio.valor_face:
             raise serializers.ValidationError({"error": "O valor oferecido não pode ser maior que o ofertado pelo credor."})
 
         return data
