76e6146a0bed044e99a874d2cda50313925ea525
Mario <marinaldo12@hotmail.com>
2025-12-12 15:52:29 -0300

    nova rota dos melhores precatórios de desagio

--- ativos/urls.py
+++ ativos/urls.py
@@ -6,6 +6,7 @@ from .views import (
     GerenciarPropostaViewAPIView,
     PrecatorioViewSet,
     PropostaPrecatorioAPIView,
+    RecomendacoesPropostaAPIView
 )
 
 router = DefaultRouter()
@@ -15,10 +16,7 @@ router.register(r"", PrecatorioViewSet, basename="precatorios")
 urlpatterns = [
     path("proposta/", PropostaPrecatorioAPIView.as_view(), name="proposta"),
     path("aprovacao/bulk/", AprovarPrecatorioBulkView.as_view(), name="aprovacao-bulk"),
-    path(
-        "gerenciar/propostas/",
-        GerenciarPropostaViewAPIView.as_view(),
-        name="gerenciar-propostas",
-    ),
+    path("gerenciar/propostas/",GerenciarPropostaViewAPIView.as_view(),name="gerenciar-propostas",),
+    path("gerenciar/propostas/recomendacoes/", RecomendacoesPropostaAPIView.as_view(), name="recomendacoes"),
     path("", include(router.urls)),
 ]
