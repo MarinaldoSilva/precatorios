7aadabe1959612736eb97a391deef935cc6d5482
Mario <marinaldo12@hotmail.com>
2025-12-15 17:01:00 -0300

    urls refatorada

--- ativos/urls.py
+++ ativos/urls.py
@@ -1,13 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (
-    AprovarPrecatorioBulkView,
-    GerenciarPropostaViewAPIView,
-    PrecatorioViewSet,
-    PropostaPrecatorioAPIView,
-    RecomendacoesPropostaAPIView
-)
+from .views import AprovarPrecatorioBulkView, GerenciarPropostaViewAPIView, PrecatorioViewSet, PropostaPrecatorioAPIView, RecomendacoesPropostaAPIView
 
 router = DefaultRouter()
 
@@ -16,7 +10,11 @@ router.register(r"", PrecatorioViewSet, basename="precatorios")
 urlpatterns = [
     path("proposta/", PropostaPrecatorioAPIView.as_view(), name="proposta"),
     path("aprovacao/bulk/", AprovarPrecatorioBulkView.as_view(), name="aprovacao-bulk"),
-    path("gerenciar/propostas/",GerenciarPropostaViewAPIView.as_view(),name="gerenciar-propostas",),
+    path(
+        "gerenciar/propostas/",
+        GerenciarPropostaViewAPIView.as_view(),
+        name="gerenciar-propostas",
+    ),
     path("gerenciar/propostas/recomendacoes/", RecomendacoesPropostaAPIView.as_view(), name="recomendacoes"),
     path("", include(router.urls)),
 ]
