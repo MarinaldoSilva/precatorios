4b1204f05b162ff7873d4f1c7f5f2079f92eb3b2
Mario <marinaldo12@hotmail.com>
2025-12-15 16:57:20 -0300

    modificação do create do user

--- user/serializer.py
+++ user/serializer.py
@@ -30,22 +30,12 @@ class UserSerializer(serializers.ModelSerializer):
     #     return user
 
     def create(self, validated_data):
-        # Print 1: Vamos ver o que chegou aqui
-        print(f"DEBUG - Validated Data Completo: {validated_data}")
-        
         password = validated_data.pop("password", None)
-        
-        # Print 2: Vamos ver se a senha foi capturada
-        print(f"DEBUG - Senha Capturada: {password}")
-
         user = User.objects.create(**validated_data)
 
         if password is not None:
-            print("DEBUG - Entrou no IF da senha. Criptografando...")
             user.set_password(password)
             user.save()
-        else:
-            print("DEBUG - PERIGO: Senha veio vazia (None)! O usuário ficará bloqueado.")
 
         return user
 
